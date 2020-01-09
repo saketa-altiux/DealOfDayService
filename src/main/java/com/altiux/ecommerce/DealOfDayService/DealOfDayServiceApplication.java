a8744071b50e7821aad2291189075aeb246c86c2
Saket Agrawal <saket.a@altiux.com>
2020-01-09 15:46:13 +0530

    Initial commit

---
+++ src/main/java/com/altiux/ecommerce/DealOfDayService/DealOfDayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.altiux.ecommerce.DealOfDayService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealOfDayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealOfDayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altiux/ecommerce/DealOfDayService/DealOfDayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altiux.ecommerce.DealOfDayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealOfDayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
